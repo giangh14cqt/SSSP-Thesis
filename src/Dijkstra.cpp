16195333faeae7ac3e2ffaed294402a6b3418738
Truong Giang Do <giangh14cqt@gmail.com>
2023-04-03 18:34:24 +0200

    Add cpp

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -1,70 +1,58 @@
-#include <iostream>
+#include "Dijkstra.h"
 #include <queue>
-#include <vector>
+#include <limits>
 
-using namespace std;
+// Constructor
+Graph::Graph(int numNodes) {
+    this->V = numNodes;
+    adj_list.resize(numNodes);
+}
 
-const int INF = INT_MAX;  // Set initial distance to infinity
+// Destructor
+Graph::~Graph() {
+    delete &adj_list;
+    delete &dist;
+}
 
-class Graph {
- private:
-  int V;                                    // Number of vertices
-  vector<vector<pair<int, int> > > adj_list;  // Adjacency list to store the graph
- public:
-  Graph(int V) {
-    this->V = V;
-    adj_list.resize(V);
-  }
+// Add a directed edge from node u to node v with weight w
+void Graph::add_edge(int u, int v, int w) {
+    adj_list[u].push_back({v, w});
+}
 
-  void add_edge(int u, int v, int w) {
-    adj_list[u].push_back(make_pair(v, w));
-    adj_list[v].push_back(
-        make_pair(u, w));  // Comment this line for directed graphs
-  }
-
-  void dijkstra(int source) {
-    vector<int> dist(V, INF);    // Distance from source to each vertex
-    vector<bool> vis(V, false);  // Visited flag for each vertex
+// Get the shortest paths from a source node to all other nodes using Dijkstra's algorithm
+void Graph::dijkstra(int source) {
+    // Initialize the distance vector with infinity for all nodes except the source
+    dist.assign(V, std::numeric_limits<int>::max());
     dist[source] = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int> >,
-                   greater<pair<int, int> > >
-        pq;
-    pq.push(make_pair(0, source));
-    while (!pq.empty()) {
-      int u = pq.top().second;
-      pq.pop();
-      if (vis[u]) continue;
-      vis[u] = true;
-      for (auto iter : adj_list[u]) {
-        auto v = iter.first;
-        auto w = iter.second;
-        if (dist[u] + w < dist[v]) {
-          dist[v] = dist[u] + w;
-          pq.push(make_pair(dist[v], v));
-        }
-      }
-    }
-    for (int i = 0; i < V; i++) {
-      cout << "Distance from " << source << " to " << i << ": ";
-      if (dist[i] == INF)
-        cout << "INF\n";
-      else
-        cout << dist[i] << '\n';
-    }
-  }
-};
 
-int main() {
-  int n, m;  // Number of vertices and edges
-  cin >> n >> m;
-  Graph g(n);
-  for (int i = 0; i < m; i++) {
-    int u, v, w;
-    cin >> u >> v >> w;
-    g.add_edge(u, v, w);
-  }
-  int source;
-  cin >> source;
-  g.dijkstra(source);
-  return 0;
+    // Create a priority queue to store nodes and their tentative distances
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+    pq.push({0, source});
+
+    // Traverse the graph using Dijkstra's algorithm
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        // Check if the current node has already been processed
+        if (dist[u] != std::numeric_limits<int>::max()) {
+            continue;
+        }
+
+        // Update the tentative distances of the neighbors of the current node
+        for (auto neighbor : adj_list[u]) {
+            int v = neighbor.first;
+            int w = neighbor.second;
+
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+
+// Return shortest distace to a vertex
+int Graph::get_shortest_distance(int vertex) {
+    return dist[vertex];
 }
