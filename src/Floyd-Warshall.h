41f30695c99c0b283dc2090ee11f0728ac56c9df
Truong Giang Do <giangh14cqt@gmail.com>
2023-04-15 11:33:13 +0200

    Format doc

--- src/Floyd-Warshall.h
+++ src/Floyd-Warshall.h
@@ -1,47 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <limits>
+#include <vector>
 
 using namespace std;
 
-const int INF = numeric_limits<int>::max(); // Set initial distance to infinity
+const int INF = numeric_limits<int>::max();  // Set initial distance to infinity
 
 class Graph {
-private:
-    int V; // Number of vertices
-    vector<vector<int>> dist;
+ private:
+  int V;  // Number of vertices
+  vector<vector<int>> dist;
 
-public:
-    // Constructor
-    Graph(int V) {
-        this->V = V;
-        dist.resize(V, vector<int>(V, INF));
-        // Initialize diagonal elements to 0
-        for (int i = 0; i < V; i++) {
-            dist[i][i] = 0;
-        }
+ public:
+  // Constructor
+  Graph(int V) {
+    this->V = V;
+    dist.resize(V, vector<int>(V, INF));
+    // Initialize diagonal elements to 0
+    for (int i = 0; i < V; i++) {
+      dist[i][i] = 0;
     }
+  }
 
-    // Add a directed edge from node u to node v with weight w
-    void add_edge(int u, int v, int w) { 
-        dist[u][v] = w; 
-    }
+  // Add a directed edge from node u to node v with weight w
+  void add_edge(int u, int v, int w) { dist[u][v] = w; }
 
-    // Execute the Floyd-Warshall algorithm
-    void floyd_warshall() {
-        for (int k = 0; k < V; k++) {
-            for (int i = 0; i < V; i++) {
-                for (int j = 0; j < V; j++) {
-                    if (dist[i][k] != INF && dist[k][j] != INF) {
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
-        }
-    }
+  // Execute the Floyd-Warshall algorithm
+  void floyd_warshall() {
+    for (int k = 0; k < V; k++)
+      for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+          if (dist[i][k] != INF && dist[k][j] != INF)
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+  }
 
-    // Return shortest distance between two vertices
-    int get_shortest_distance(int u, int v) { 
-        return dist[u][v]; 
-    }
+  // Return shortest distance between two vertices
+  int get_shortest_distance(int u, int v) { return dist[u][v]; }
 };
