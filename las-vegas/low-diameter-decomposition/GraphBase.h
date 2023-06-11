b2ca9e6a6b6c40d6116486d347828aa1a927797d
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-12 00:22:08 +0200

    Add code for LDD

---
+++ las-vegas/low-diameter-decomposition/GraphBase.h
@@ -0,0 +1,183 @@
+#ifndef GRAPHBASE_H
+#define GRAPHBASE_H
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+#define ii pair<int, int> // (node, distance_from_source) or (node, weight)
+#define iii tuple<int, int, int> // (node_u, node_v, weight)
+
+using namespace std;
+
+const int INF = INT_MAX; // Set initial distance to infinity
+
+enum class Weight
+{
+    NONE,
+    IN_LIGHT,
+    OUT_LIGHT,
+    HEAVY
+};
+
+class Graph
+{
+private:
+    // number of nodes in the (origin) graph
+    int V;
+    // list of edges in the (origin) graph by adjacency list
+    vector<vector<ii>> adj_list;
+    // list of reversed edges in the (origin) graph by adjacency list
+    vector<vector<ii>> adj_list_rev;
+    // list of active nodes in the current graph
+    vector<int> active_nodes;
+
+public:
+/**
+ * @brief Construct a new Graph object
+ * @param V The number of nodes in the graph
+*/
+    Graph(int V)
+    {
+        this->V = V;
+        adj_list.resize(V);
+        adj_list_rev.resize(V);
+        active_nodes.resize(V, 1);
+        for (int i = 0; i < V; i++)
+        {
+            active_nodes[i] = i;
+        }
+    }
+
+/**
+ * @brief Construct a new Graph object from a given graph and a set of active nodes
+ * @param G The origin graph
+ * @param active_nodes_ The set of active nodes
+ * @return Graph based on the origin graph and the set of active nodes
+*/
+    Graph(Graph G, vector<int> active_nodes_)
+    {
+        this->V = G.get_V();
+        this->active_nodes = active_nodes_;
+        this->adj_list = G.get_adj_list();
+        this->adj_list_rev = G.get_adj_list_rev();
+    }
+
+/***
+ * @brief Add an edge to the graph
+ * @param u The node from
+ * @param v The node to
+ * @param w The weight of the edge
+*/
+    void add_edge(int u, int v, int w)
+    {
+        adj_list[u].push_back({v, w});
+        adj_list_rev[v].push_back({u, w});
+    }
+
+/**
+ * @brief Get the number of nodes in the graph
+ * @return The number of nodes in the graph
+*/
+    int get_V()
+    {
+        return V;
+    }
+
+/**
+ * @brief Get the list of active nodes
+*/
+    vector<int> get_active_nodes()
+    {
+        return active_nodes;
+    }
+
+/**
+ * @brief Get the size of the list of active nodes
+*/
+    int get_active_size()
+    {
+        return active_nodes.size();
+    }
+
+/**
+ * @brief Get the list of 
+*/
+    vector<vector<pair<int, int>>> get_adj_list()
+    {
+        return adj_list;
+    }
+
+/**
+ * @brief Get the reversed edges
+*/
+    vector<vector<pair<int, int>>> get_adj_list_rev()
+    {
+        return adj_list_rev;
+    }
+
+/**
+ * @brief Remove nodes from the graph by removing them from the list of active nodes
+ * @param ball The set of nodes to be removed
+*/
+    void remove(vector<int> &ball)
+    {
+        vector<int> new_active_nodes;
+        for (int i = 0; i < active_nodes.size(); i++)
+        {
+            if (binary_search(ball.begin(), ball.end(), active_nodes[i]))
+                continue;
+            new_active_nodes.push_back(active_nodes[i]);
+        }
+        active_nodes = new_active_nodes;
+    }
+
+/**
+ * @brief Check if a node is active
+ * @param v The node to be checked
+ * @return True if the node is active, false otherwise
+*/
+    bool is_active(int v)
+    {
+        return binary_search(active_nodes.begin(), active_nodes.end(), v);
+    }
+
+/**
+ * @brief Clone a graph
+ * @return A clone of the graph
+*/
+    Graph clone()
+    {
+        Graph G(V);
+        G.adj_list = adj_list;
+        G.adj_list_rev = adj_list_rev;
+        G.active_nodes = active_nodes;
+        return G;
+    }
+
+/**
+ * @brief Get the list of edges as a vector of tuples (u, v, w) - from node u to node v with weight w
+ * @return The list of edges as a vector of tuples (u, v, w)
+*/
+    vector<iii> get_adj_list_as_iii()
+    {
+        vector<iii> adj_list_as_iii;
+        for (int u = 0; u < V; u++)
+        {
+            if (!is_active(u))
+                continue;
+            for (auto [v, w] : adj_list[u])
+            {
+                if (!is_active(v) || v > u)
+                    continue;
+                adj_list_as_iii.push_back({u, v, w});
+            }
+        }
+        return adj_list_as_iii;
+    }
+};
+
+#endif // GRAPHBASE_H
---
+++ las-vegas/low-diameter-decomposition/LDD.h
@@ -0,0 +1,254 @@
+#include "GraphBase.h"
+
+const int C = 100; // constant for LDD
+
+// Randomized algorithm low diameter decomposition with radius r, return the list of Erem
+// The implementation is based on 2203.03456 page 16
+vector<iii> LDD(Graph &G, int d)
+{
+    // Set up
+    std::srand(std::time(nullptr)); // seed the random number generator
+    // line 1
+    int n = G.get_V();
+    vector<Weight> weight(n, Weight::NONE);
+    // line 2
+    vector<iii> erem;
+    Graph G0 = G.clone();
+    // line 3
+    int k = C * log1p(n);
+    // line 4
+    vector<int> active_nodes = G.get_active_nodes();
+    vector<int> S;
+    for (int i = 0; i < k; i++)
+    {
+        int v = rand() % active_nodes.size();
+        S.push_back(active_nodes[v]);
+    }
+    // line 5
+    vector<vector<int>> ball_in(n, vector<int>());
+    vector<vector<int>> ball_out(n, vector<int>());
+    for (int si : S)
+    {
+        ball_in[si] = computer_ball_in(G, si, d / 4);
+        ball_out[si] = computer_ball_out(G, si, d / 4);
+    }
+    // line 6
+    vector<vector<int>> ball_in_cross(n, vector<int>());
+    vector<vector<int>> ball_out_cross(n, vector<int>());
+    for (int v : active_nodes)
+    {
+        ball_in_cross[v] = get_cross(computer_ball_in(G, v, d / 4), S);
+        ball_out_cross[v] = get_cross(computer_ball_out(G, v, d / 4), S);
+    }
+    // line 7
+    for (int v : active_nodes)
+    {
+        // line 8
+        if (ball_in_cross[v].size() <= 6 * k)
+        {
+            weight[v] = Weight::IN_LIGHT;
+        }
+        // line 9
+        else if (ball_out_cross[v].size() <= 6 * k)
+        {
+            weight[v] = Weight::OUT_LIGHT;
+        }
+        // line 10
+        else
+        {
+            weight[v] = Weight::HEAVY;
+        }
+    }
+    // line 11
+    double p = min(1.0, 80 * log2(n) / d);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::geometric_distribution<int> distr(p);
+    int v = contain_light(weight);
+
+    while (v != -1)
+    {
+        // line 12
+        double rv = distr(gen);
+        // line 13
+        vector<int> ball_in_rv = computer_ball_in(G, v, rv);
+        vector<int> ball_out_rv = computer_ball_out(G, v, rv);
+        // line 14
+        vector<iii> e_boundary;
+        e_boundary = get_boundary(G, ball_in_rv, ball_out_rv);
+        // line 15
+        if (rv > d / 4.0 ||
+            ball_in_rv.size() > 7 * G.get_active_nodes().size())
+        {
+            // return G.get_adj_list();
+            return G.get_adj_list_as_iii();
+        }
+        // line 16
+        vector<iii> e_recurse;
+        Graph G_ball = Graph(G, get_cross(ball_in_rv, ball_out_rv));
+        e_recurse = LDD(G_ball, d);
+        // line 17
+        get_cross_e(erem, e_boundary, e_recurse);
+        // line 18
+        G.remove(ball_in_rv);
+        G.remove(ball_out_rv);
+
+        v = contain_light(weight);
+    }
+    // line 19
+    v = G.get_active_nodes()[rand() % G.get_active_nodes().size()];
+    // line 20
+    vector<int> ball_in_g0 = computer_ball_in(G0, v, d / 2);
+    vector<int> ball_out_g0 = computer_ball_out(G0, v, d / 2);
+    if (is_sub_set(ball_in_g0, G.get_active_nodes()) ||
+        is_sub_set(ball_out_g0, G.get_active_nodes()))
+    {
+        // return G.get_adj_list();
+        return G.get_adj_list_as_iii();
+    }
+
+    return erem;
+}
+
+vector<int> get_cross(vector<int> a, vector<int> b)
+{
+    a.insert(a.end(), b.begin(), b.end());
+    sort(a.begin(), a.end());
+    vector<int>::iterator it;
+    it = unique(a.begin(), a.end());
+    a.resize(distance(a.begin(), it));
+    return a;
+}
+
+void get_cross_e(vector<iii> &erem, vector<iii> eboundary, vector<iii> erecurse)
+{
+    erem.insert(erem.end(), eboundary.begin(), eboundary.end());
+    erem.insert(erem.end(), erecurse.begin(), erecurse.end());
+    sort(erem.begin(), erem.end());
+    vector<iii>::iterator it;
+    it = unique(erem.begin(), erem.end());
+    erem.resize(distance(erem.begin(), it));
+}
+
+vector<iii> get_boundary(Graph &G, vector<int> &ball_in, vector<int> &ball_out)
+{
+    // TODO: check again
+    // vector<vector<ii>> boundary(G.get_V(), vector<ii>());
+    vector<iii> boundary;
+    for (int u : G.get_active_nodes())
+    {
+        for (auto inter : G.get_adj_list()[u])
+        {
+            auto v = inter.first;
+            if (!G.is_active(v))
+                continue;
+
+            auto w = inter.second;
+
+            if (is_in(u, ball_out) && !is_in(v, ball_out))
+            {
+                // boundary[u].push_back(make_pair(v, w));
+                if (u < v)
+                    boundary.push_back({u, v, w});
+                else
+                    boundary.push_back({v, u, w});
+            }
+            else if (!is_in(u, ball_in) && is_in(v, ball_in))
+            {
+                // boundary[u].push_back(make_pair(v, w));
+                if (u < v)
+                    boundary.push_back({u, v, w});
+                else
+                    boundary.push_back({v, u, w});
+            }
+        }
+    }
+    return boundary;
+}
+
+int contain_light(vector<Weight> &weight)
+{
+    for (int i = 0; i < weight.size(); i++)
+        if (weight[i] == Weight::IN_LIGHT || weight[i] == Weight::OUT_LIGHT)
+            return i;
+    return -1;
+}
+
+vector<int> computer_ball_in(Graph &G, int s, int d)
+{
+    int n = G.get_V();
+    vector<vector<ii>> *adj_list = &G.get_adj_list();
+    vector<int> ball_in;
+    vector<int> dist(n, INF);
+    priority_queue<ii, vector<ii>, greater<ii>> pq;
+    dist[s] = 0;
+    pq.push(make_pair(0, s));
+    while (!pq.empty())
+    {
+        int w = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+        if (dist[u] > d)
+            break;
+        ball_in.push_back(u);
+        for (auto iter : (*adj_list)[u])
+        {
+            auto v = iter.first;
+            if (!G.is_active(v))
+                continue;
+            auto w = iter.second;
+            if (dist[u] + w < dist[v])
+            {
+                dist[v] = dist[u] + w;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    return ball_in;
+}
+
+vector<int> computer_ball_out(Graph &G, int s, int d)
+{
+    int n = G.get_V();
+    vector<vector<ii>> *adj_list_rev = &G.get_adj_list_rev();
+    vector<int> ball_out;
+    vector<int> dist(n, INF);
+    priority_queue<ii, vector<ii>, greater<ii>> pq;
+    dist[s] = 0;
+    pq.push(make_pair(0, s));
+    while (!pq.empty())
+    {
+        int w = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+        if (dist[u] > d)
+            break;
+        ball_out.push_back(u);
+        for (auto iter : (*adj_list_rev)[u])
+        {
+            auto v = iter.first;
+            if (!G.is_active(v))
+                continue;
+            auto w = iter.second;
+            if (dist[u] + w < dist[v])
+            {
+                dist[v] = dist[u] + w;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    return ball_out;
+}
+
+bool is_sub_set(const vector<int> &a, const vector<int> &b)
+{
+    for (int i : a)
+        if (find(b.begin(), b.end(), i) == b.end())
+            return false;
+    return true;
+}
+
+bool is_in(int x, const vector<int> &a)
+{
+    return find(a.begin(), a.end(), x) != a.end();
+}
